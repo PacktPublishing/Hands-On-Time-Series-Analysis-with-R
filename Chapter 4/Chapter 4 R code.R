88ad98f30ec7684b6f8e1192c12d47674619cb3d
RamiKrispin <ramkrisp@umich.edu>
2018-10-21 23:25:20 -0700

    Updated the chapter code

--- Chapter 4/Chapter 4 R code.R
+++ Chapter 4/Chapter 4 R code.R
@@ -126,5 +126,114 @@ class(Michigan_CS)
 
 head(Michigan_CS)
 # -------- Code Chank 16 --------
+plot.xts(xts_mts_obj,
+         multi.panel = 2, # Use separate the plots, as the two series have different scale 
+         yaxis.same = FALSE, # For the same reason, adjust the yaxis to the scale of each series
+         grid.ticks.on = "years",  # Setting the grid ticks period to draw
+         main = "Monthly Vehicle Sales and Unemployment Rate in the US ")
+# -------- Code Chank 17 --------
+start(xts_mts_obj)
 
-# -------- Code Chank 1 --------
+end(xts_mts_obj)
+
+frequency(xts_mts_obj)
+# -------- Code Chank 18 --------
+# Getting the series frequency/time interval and the starting and ending dates
+periodicity(xts_mts_obj)
+# -------- Code Chank 19 --------
+# Working with the xts index
+head(index(xts_mts_obj))
+
+indexClass(xts_mts_obj)
+
+# Modifing the series index from yearmon to Date
+xts_mts_obj <- convertIndex(xts_mts_obj, "Date")
+
+indexClass(xts_mts_obj)
+
+head(xts_mts_obj)
+# -------- Code Chank 20 --------
+# Extracting the first 12 months of the vehicle sales dataset
+class(xts_mts_obj)
+
+names(xts_mts_obj)
+
+# Extracting the first 12 months of the vehicle sales dataset
+# using the column name and the row index
+vehical_first_12m <- xts_mts_obj$`Vehicle Sales`[1:12]
+
+vehical_first_12m
+# -------- Code Chank 21 --------
+# Checking the new series attributes
+class(vehical_first_12m)
+
+periodicity(vehical_first_12m)
+
+is.regular(vehical_first_12m)
+# -------- Code Chank 22 --------
+# Extracing the first 12 months of the unemployment rate
+# and assign it as a new variable in the xts object
+vehical_first_12m$Unemployment.Rate <- xts_mts_obj[1:12, 2]
+
+vehical_first_12m
+
+class(vehical_first_12m)
+# -------- Code Chank 23 --------
+# Extracting all the observations during the year 1976
+US_indicators_xts1 <- xts_mts_obj["1976"]
+
+class(US_indicators_xts1)
+
+US_indicators_xts1
+# -------- Code Chank 24 --------
+# Extracting all the observations of the year 1976
+US_indicators_xts2 <- xts_mts_obj["1976-01/1976-12"]
+
+# A more concise method 
+US_indicators_xts3 <- xts_mts_obj["197601/12"]
+
+# Which eventually yield the same results:
+identical(US_indicators_xts1, US_indicators_xts2, US_indicators_xts3)
+# -------- Code Chank 25 --------
+data("USgas", package = "TSstudio")
+periodicity(USgas)
+class(USgas)
+
+periodicity(xts_mts_obj)
+class(xts_mts_obj)
+# -------- Code Chank 26 --------
+# Merging the xts_mts_obj and the USgas series
+# By default, the merge function applying an outer join
+US_ind <- merge(x = xts_mts_obj, USgas = USgas)
+class(US_ind)
+periodicity(US_ind)
+# -------- Code Chank 27 --------
+head(US_ind)
+# -------- Code Chank 28 --------
+# Applying inner join
+US_ind_inner <- merge(x = xts_mts_obj, USgas = USgas, join = "inner")
+class(US_ind_inner)
+periodicity(US_ind_inner)
+head(US_ind_inner)
+tail(US_ind_inner)
+# -------- Code Chank 29 --------
+plot.xts(US_ind_inner,
+         multi.panel = 3, # Use separate the plots, as the two series have different scale 
+         yaxis.same = FALSE, # For the same reason, adjust the yaxis to the scale of each series
+         grid.ticks.on = "years", # Setting the grid ticks period to draw 
+         main = "Monthly Vehicle Sales, Unemployment Rate and Natural Gas Consumption in the US ")
+# -------- Code Chank 31 --------
+# Loading the US vehicle sales series
+data(USVSales, package = "TSstudio")
+
+# Applying a moving average of 3 months rolling window 
+USVSales_smooth <- rollapply(USVSales, FUN = mean, width = 3)
+
+# Merging the two series
+# To apply the xts merge method, we will convert the objects to xts
+USVSales_merged <- merge(Normal = as.xts(USVSales), Smooth = as.xts(USVSales_smooth), join = "inner")
+
+plot.xts(USVSales_merged, 
+         multi.panel = 2,
+         grid.ticks.on = "years",
+         main = "The US Vehicale Sales Before and After Smoothing")
